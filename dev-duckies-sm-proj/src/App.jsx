1c19d2d02ffec6daa05cc9bb06a1b6d9ad88af00
leo <leomasellis.cohort238@gmail.com>
2024-04-11 21:10:17 -0700

    first registration page commit

--- dev-duckies-sm-proj/src/App.jsx
+++ dev-duckies-sm-proj/src/App.jsx
@@ -1,12 +1,40 @@
-import './App.css';
-function App() {
-return (
-<section className="section">
- <div className="container">
-   <h1 className="title"> Hello World </h1>
-   <p className="subtitle"> My React app  with     <strong>Bulma</strong> </p>
- </div>
-</section>
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleChange = (event) => {
+        setState(event.target.value);
+        console.log(state)
+    };
+
+  return(
+    <main>
+      <div className='card-container'>
+       <div className="register-card">
+
+        <input className="email-box"
+               type="text" 
+               placeholder='Email Address'>
+        </input>
+
+        <input className="password-box"
+               type="text" 
+               placeholder='Password'>
+        </input>
+
+        <input className="password-confirm-box"
+               type="text"
+               placeholder='Confirm Password'>
+        </input>
+        <button>Create Account</button>
+       </div>
+      </div>
+    </main>
+  )
 }
-export default App;
