e3d594c90ba20a6f7f32acc46db9eec45658f096
Jose Lema <jose.lema.2310@gmail.com>
2024-04-16 16:21:32 -0700

    Updated Card with static posts as example

--- dev-duckies-sm-proj/src/app-wide-components/post.jsx
+++ dev-duckies-sm-proj/src/app-wide-components/post.jsx
@@ -1,26 +1,28 @@
+import 'bulma/css/bulma.css'
+
 export default function Post(props) {
     return (
         <div>
-            <div class="card">
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-48x48">
+            <div className="card">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <figure className="image is-48x48">
                                 <img
                                     src="https://bulma.io/assets/images/placeholders/96x96.png"
                                     alt="Placeholder image"
                                 />
                             </figure>
                         </div>
-                        <div class="media-content">
-                            <p class="title is-4">{props.name}</p>
+                        <div className="media-content">
+                            <p className="title is-4">{props.name}</p>
                         </div>
                     </div>
 
-                    <div class="content">
+                    <div className="content">
                         <p>{props.postContent}</p>
                         <br />
-                        <time datetime={props.date}>{props.date} {props.time}</time>
+                        <time>{props.date} {props.time}</time>
                     </div>
                 </div>
             </div>
