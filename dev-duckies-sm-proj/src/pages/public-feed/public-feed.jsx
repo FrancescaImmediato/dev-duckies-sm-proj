7eaacf6e9d231a97f3a54485e52fd86775f4f53b
Jose-Lema <jose.lema.2310@gmail.com>
2024-05-02 13:48:18 -0700

    Prototype of Feed Page

--- dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
+++ dev-duckies-sm-proj/src/pages/public-feed/public-feed.jsx
@@ -1,10 +1,51 @@
-export default function PublicFeed() {
+// App.js
+import React from "react";
+import Header from "./Header";
+import Post from "./Post";
+import Sidebar from "./Sidebar";
+import "./styles.css";
+
+function PublicFeed() {
+  // Sample data for posts
+  const posts = [
+    // { id: 1, user: "User1", content: "This is the first post" },
+    // { id: 2, user: "User2", content: "This is the second post" },
+    // // Add more posts as needed
+  ];
+
   return (
-    <section className="section">
-      <h1 className="title">Feed</h1>
-      <h2 className="subtitle">
-        A home/feed/explore page for all posts by all users.
-      </h2>
-    </section>
-  )
+    <div className="App">
+      <Header />
+      <div className="columns">
+        <div className="column is-4">
+          <Sidebar />
+        </div>
+        <div className="column is-8">
+          {/* {posts.map((post) => (
+            <Post key={post.id} user={post.user} content={post.content} />
+          ))} */}
+          <Post
+            user="Tommy"
+            postContent="Testing 123"
+            date="10/3/24"
+            time="13:14"
+          />
+          <Post
+            user="Jessie"
+            postContent="Testing 123"
+            date="10/3/24"
+            time="13:14"
+          />
+          <Post
+            user="Albert"
+            postContent="Testing 123"
+            date="10/3/24"
+            time="13:14"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default PublicFeed;
