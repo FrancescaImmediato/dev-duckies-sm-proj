7bf9ce31a365ec098bb9c86785fdf53088ae5c7e
Jose-Lema <jose.lema.2310@gmail.com>
2024-05-22 15:52:49 -0700

    Fixed Comment Modal Issues

--- dev-duckies-sm-proj/src/app-wide-components/post.jsx
+++ dev-duckies-sm-proj/src/app-wide-components/post.jsx
@@ -1,11 +1,10 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+import { useState } from 'react';
 
 const Post = (props) => {
 
-  const handleComment = () =>{
-    console.log('I made a comment!')
-  }
-  
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="box">
       <div className="media m-auto">
@@ -33,7 +32,7 @@ const Post = (props) => {
         <div className="level-left">
           <a className="level-item" aria-label="comment">
             <span className="icon is-small">
-              <button className='button is-borderless m-0 p-0' id='comment' onClick={handleComment}>
+              <button className='button is-borderless m-0 p-0' id='comment' onClick={() => setIsOpen(true)}>
                 <i className="fas fa-comment"></i>
               </button>
             </span>
@@ -45,6 +44,24 @@ const Post = (props) => {
           </a>
         </div>
       </nav>
+      <div className={`modal ${isOpen ? 'is-active' : ''}`}>
+        <div className="modal-background" onClick={() => setIsOpen(false)}></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Write a comment!</p>
+            <button className="delete" aria-label="close" onClick={() => setIsOpen(false)}></button>
+          </header>
+          <section className="modal-card-body">
+            <textarea className='textarea'></textarea>
+          </section>
+          <footer className="modal-card-foot">
+            <div className="buttons">
+              <button className="button is-success mt-1">Post</button>
+              <button className='button is-ghost mt-1' onClick={() => setIsOpen(false)}>Cancel</button>
+            </div>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 };
