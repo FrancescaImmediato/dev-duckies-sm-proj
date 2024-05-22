35aa7a832dc30aa54cd8cbcebe36553bc26f9846
Jose-Lema <jose.lema.2310@gmail.com>
2024-05-22 16:05:50 -0700

    Created Like list modal

--- dev-duckies-sm-proj/src/app-wide-components/post.jsx
+++ dev-duckies-sm-proj/src/app-wide-components/post.jsx
@@ -3,7 +3,8 @@ import { useState } from 'react';
 
 const Post = (props) => {
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [commentIsOpen, setCommentIsOpen] = useState(false);
+  const [likeIsOpen, setLikeIsOpen] = useState(false);
 
   return (
     <div className="box">
@@ -32,32 +33,53 @@ const Post = (props) => {
         <div className="level-left">
           <a className="level-item" aria-label="comment">
             <span className="icon is-small">
-              <button className='button is-borderless m-0 p-0' id='comment' onClick={() => setIsOpen(true)}>
+              <button className='button is-borderless m-0 p-0' id='comment' onClick={() => setCommentIsOpen(true)}>
                 <i className="fas fa-comment"></i>
               </button>
             </span>
           </a>
           <a className="level-item" aria-label="like">
             <span className="icon is-small">
-              <i className="fas fa-heart" aria-hidden="true"></i>
+              <button className='button is-borderless m-0 p-0' id='like' onClick={() => setLikeIsOpen(true)}>
+                <i className="fas fa-heart"></i>
+              </button>
             </span>
           </a>
         </div>
       </nav>
-      <div className={`modal ${isOpen ? 'is-active' : ''}`}>
-        <div className="modal-background" onClick={() => setIsOpen(false)}></div>
+      <div className={`modal ${commentIsOpen ? 'is-active' : ''}`}>
+        <div className="modal-background" onClick={() => setCommentIsOpen(false)}></div>
         <div className="modal-card">
           <header className="modal-card-head">
             <p className="modal-card-title">Write a comment!</p>
-            <button className="delete" aria-label="close" onClick={() => setIsOpen(false)}></button>
+            <button className="delete" aria-label="close" onClick={() => setCommentIsOpen(false)}></button>
           </header>
           <section className="modal-card-body">
-            <textarea className='textarea'></textarea>
+            <textarea className='textarea' id='comments'></textarea>
           </section>
           <footer className="modal-card-foot">
             <div className="buttons">
               <button className="button is-success mt-1">Post</button>
-              <button className='button is-ghost mt-1' onClick={() => setIsOpen(false)}>Cancel</button>
+              <button className='button is-ghost mt-1' onClick={() => setCommentIsOpen(false)}>Cancel</button>
+            </div>
+          </footer>
+        </div>
+      </div>
+      <div className={`modal ${likeIsOpen ? 'is-active' : ''}`}>
+        <div className="modal-background" onClick={() => setLikeIsOpen(false)}></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">See who likes this post!</p>
+            <button className="delete" aria-label="close" onClick={() => setLikeIsOpen(false)}></button>
+          </header>
+          <section className="modal-card-body">
+            <ul>
+              <li>YOU</li>
+            </ul>
+          </section>
+          <footer className="modal-card-foot">
+            <div className="buttons">
+              <button className='button is-success mt-1' onClick={() => setLikeIsOpen(false)}>Close</button>
             </div>
           </footer>
         </div>
